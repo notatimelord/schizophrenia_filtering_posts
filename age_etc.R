760c2e5ba5cab8ead233412c106b89b528cc38ee
notatimelord <150253431+notatimelord@users.noreply.github.com>
2024-12-08 18:08:29 +0200

    Update age_etc.R

--- age_etc.R
+++ age_etc.R
@@ -15,7 +15,7 @@ data <- data %>%
       IQ > 90 & IQ < 110 ~ "90<IQ<110",
       TRUE ~ "IQ>=110"
     ),
-    education_category = ifelse(education <= 16, "Educ.<=16", "Educ.>16")
+    education_category = ifelse(education <= 12, "Educ.<=12", "Educ.>12")
   )
 
 data <- data %>%
@@ -45,20 +45,20 @@ graph <- graph_from_data_frame(edges, directed = FALSE, vertices = nodes)
 
 E(graph)$normalized_weight <- (E(graph)$weight - min(E(graph)$weight)) / (max(E(graph)$weight) - min(E(graph)$weight))
 
-cat("Edge Weights for Each Combination:\n")
+cat("Healthy vs Schizophrenic:\n")
 for (i in 1:nrow(edges)) {
   cat(paste(edges$from[i], "-", edges$to[i], ": Weight =", edges$weight[i]), "\n")
 }
 
 layout <- layout_with_fr(graph)
 
-plot(graph,
-     layout = layout, 
-     vertex.size = 30,
-     vertex.label.cex = 1.2,
-     vertex.label.color = "black",
-     vertex.label.dist = 1,
-     edge.width = E(graph)$normalized_weight * 5,
-     edge.color = "gray",
-     main = "Simplified Network of Group Combinations"
-)
+plot(graph, 
+     vertex.size = 15,  # Adjust size of nodes
+     vertex.label.cex = 0.8,  # Adjust the size of node labels
+     vertex.label.dist = 1.5,  # Distance of label from the node
+     vertex.color = "skyblue",  # Color of nodes
+     edge.width = E(graph)$normalized_weight * 5,  # Edge width based on the normalized weight
+     edge.color = "gray",  # Color of edges
+     layout = layout,  # Use force-directed layout
+     main = "Simplified Network of Group Combinations with Weights as Edge Thickness")  # Title of the graph
+
